e4c1c3480ce7889c2ce28d5fc9d25d3a96833500
Benjamin <brcj16@gmail.com>
2026-05-28 10:55:32 +0200

    pokeApi

--- modulo.js
+++ modulo.js
@@ -1,4 +1,4 @@
-import { contador, promesa, lecturaPromesa } from './script.js';
+import { contador, promesa, lecturaPromesa, pokeApi } from './script.js';
 
 const counter = document.getElementById('contador');
 
@@ -6,3 +6,4 @@ contador(counter)
 console.log(await promesa())
 
 lecturaPromesa()
+pokeApi()
--- script.js
+++ script.js
@@ -14,7 +14,7 @@ export async function contador(counter) {
 
 }
 
-const parametro = false;
+const parametro = true;
 export function promesa() {
     return new Promise((resolve, reject) => {
         if (parametro) {
@@ -30,4 +30,23 @@ export function lecturaPromesa() {
         .then(response => console.log(response + ' desde lectura'))
         .catch(error => console.log(error))
         .finally(() => console.log('ejecutado'))
+}
+
+
+export async function pokeApi() {
+    let contador = 0;
+    setInterval(async () => {
+        contador++
+        const Api = await fetch(`https://pokeapi.co/api/v2/pokemon/${contador}`);
+        const imgJson = await Api.json()
+
+        const pokeImg = document.getElementById('poke');
+        console.log(imgJson.sprites.other['official-artwork'].front_default); //para leer el - del json necesito ['']
+        pokeImg.src = imgJson.sprites.other['official-artwork'].front_default;
+        if (contador >= 151) {
+            contador = 1
+        }
+    }, 1000)
+
+
 }
